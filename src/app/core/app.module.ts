3f291a22dc8c54024ed1233332ab165cfd92a783
srinivasallanki <srinivas5859@gmail.com>
2019-05-14 17:58:00 +0530

    bootstrap basic skletion

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from '../components/home/home.component';
+import { PortfolioComponent } from '../components/portfolio/portfolio.component';
+import { ContactComponent } from '../components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
